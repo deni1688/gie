f1d0908998406a2669e40249f450c214134b527e
Denis <--list>
2022-10-10 15:46:30 +0200

    implements the notify method for webhooks
    closes #28

--- infra/webhook.go
+++ infra/webhook.go
@@ -1,8 +1,11 @@
 package infra
 
 import (
+	"bytes"
 	"deni1688/gogie/internal/issues"
+	"encoding/json"
 	"fmt"
+	"net/http"
 )
 
 type webhookNotifier struct {
@@ -10,14 +13,38 @@ type webhookNotifier struct {
 	client   HttpClient
 }
 
-// Todo: Implement Notify for webhooks -> https://github.com/deni1688/gogie/issues/28
-func (r webhookNotifier) Notify(issues *[]issues.Issue) error {
-	for _, webhook := range r.webhooks {
-		fmt.Printf("Sending to webhook: %s\n", webhook)
-	}
-	return nil
-}
-
 func NewWebhookNotifier(webhooks []string, client HttpClient) issues.Notifier {
 	return &webhookNotifier{webhooks, client}
+}
+
+func (r webhookNotifier) Notify(issues *[]issues.Issue) error {
+	if len(r.webhooks) < 1 {
+		return nil
+	}
+
+	var (
+		err  error
+		req  *http.Request
+		resp *http.Response
+	)
+
+	issuesJson, err := json.Marshal(issues)
+	if err != nil {
+		return err
+	}
+
+	for _, webhook := range r.webhooks {
+		req, err = http.NewRequest("POST", webhook, bytes.NewBuffer(issuesJson))
+		if err != nil {
+			return err
+		}
+
+		req.Header.Set("Content-Type", "application/json")
+		resp, err = r.client.Do(req)
+		if err != nil || resp.StatusCode >= 400 {
+			fmt.Printf("Error sending webhook %s %s\n", webhook, err)
+		}
+	}
+
+	return nil
 }
