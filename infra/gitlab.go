6e024d2cd9ff7d267add3574a4f950dafb980b89
Denis <--list>
2022-10-10 10:58:47 +0200

    implements the gitlab provider for creating issues

--- infra/gitlab.go
+++ infra/gitlab.go
@@ -1,8 +1,11 @@
 package infra
 
 import (
+	"bytes"
 	"deni1688/gogie/internal/issues"
 	"encoding/json"
+	"fmt"
+	"io"
 	"net/http"
 )
 
@@ -11,6 +14,13 @@ type gitlab struct {
 	host   string
 	query  string
 	client HttpClient
+}
+
+type gitlabIssue struct {
+	ID     int    `json:"id"`
+	Title  string `json:"title"`
+	Desc   string `json:"description"`
+	WebUrl string `json:"web_url"`
 }
 
 func NewGitlab(token, host, query string, client HttpClient) issues.GitProvider {
@@ -39,6 +49,35 @@ func (r gitlab) GetRepos() (*[]issues.Repo, error) {
 
 // Todo: Implement the CreateIssue method for Gitlab -> https://github.com/deni1688/gogie/issues/27
 func (r gitlab) CreateIssue(repo *issues.Repo, issue *issues.Issue) error {
+	req, err := r.request("POST", fmt.Sprintf("projects/%d/issues", repo.ID))
+	if err != nil {
+		return err
+	}
+
+	body, err := json.Marshal(gitlabIssue{Title: issue.Title, Desc: issue.Desc})
+	if err != nil {
+		return err
+	}
+
+	req.Body = io.NopCloser(bytes.NewReader(body))
+
+	resp, err := r.client.Do(req)
+	if err != nil {
+		return err
+	}
+
+	if resp.StatusCode != http.StatusCreated {
+		return fmt.Errorf("unexpected status code: %d", resp.StatusCode)
+	}
+
+	var createdIssue gitlabIssue
+	if err = json.NewDecoder(resp.Body).Decode(&createdIssue); err != nil {
+		return err
+	}
+
+	issue.ID = createdIssue.ID
+	issue.Url = createdIssue.WebUrl
+
 	return nil
 }
 
