38562b90130a0635867029e5c3bda121c35d6a32
Denis <--list>
2022-10-14 18:01:50 +0200

    fixes test fail

--- common/logger.go
+++ common/logger.go
@@ -8,10 +8,11 @@ type Logger struct {
 }
 
 func (l Logger) Error(err error, content ...any) error {
-	log.Default().Println("\033[31m", content, err)
+	log.Default().Println(err)
+	log.Default().Println(content...)
 	return err
 }
 
 func (l Logger) Info(content ...any) {
-	log.Default().Println(content)
+	log.Default().Println(content...)
 }
