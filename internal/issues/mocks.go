e1db3232ec3fb38de8e4c8538f627ce4c894f351
Denis <denis.junuzovic@fleetster.net>
2022-10-17 15:00:41 +0200

    extracts mocks to own file

---
+++ internal/issues/mocks.go
@@ -0,0 +1,25 @@
+package issues
+
+type mockGitProvider struct {
+	repos []Repo
+	issue Issue
+	err   error
+}
+
+func (m *mockGitProvider) GetRepos() (*[]Repo, error) {
+	return &m.repos, m.err
+}
+
+func (m *mockGitProvider) CreateIssue(repo *Repo, issue *Issue) error {
+	issue.ID = m.issue.ID
+	issue.Url = m.issue.Url
+	return m.err
+}
+
+type mockNotifier struct {
+	err error
+}
+
+func (m mockNotifier) Notify(issues *[]Issue) error {
+	return m.err
+}
--- internal/issues/service_test.go
+++ internal/issues/service_test.go
@@ -6,30 +6,6 @@ import (
 	"testing"
 )
 
-type mockGitProvider struct {
-	repos []Repo
-	issue Issue
-	err   error
-}
-
-func (m *mockGitProvider) GetRepos() (*[]Repo, error) {
-	return &m.repos, m.err
-}
-
-func (m *mockGitProvider) CreateIssue(repo *Repo, issue *Issue) error {
-	issue.ID = m.issue.ID
-	issue.Url = m.issue.Url
-	return m.err
-}
-
-type mockNotifier struct {
-	err error
-}
-
-func (m mockNotifier) Notify(issues *[]Issue) error {
-	return m.err
-}
-
 func TestNew(t *testing.T) {
 	type args struct {
 		gitProvider GitProvider
